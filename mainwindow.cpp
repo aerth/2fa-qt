3439fcb37309b308ace50b99217cb6a172a1b1a2
aerth <aerth@riseup.net>
2020-04-11 02:55:38 -0400

    "add" function

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,6 +8,16 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    this->repopulate();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::repopulate(void)
+{
     // get list of OTP names
     QProcess process;
     process.start("2fa -list");
@@ -22,15 +32,12 @@ MainWindow::MainWindow(QWidget *parent) :
     } else {
     // populate combobox
     QStringList items = cmd_stdout.split(QRegExp("[\r\n]"),QString::SkipEmptyParts);
+    if (ui->comboBox->count() > 0) {
+        ui->comboBox->clear();
+    }
     ui->comboBox->addItems(items);
     this->currentKey = items[0];
     }
-
-}
-
-MainWindow::~MainWindow()
-{
-    delete ui;
 }
 
 void MainWindow::on_comboBox_activated(const QString &arg1)
@@ -50,7 +57,7 @@ void MainWindow::on_pushButton_released()
     // get OTP
     QProcess process;
     process.start("2fa " + this->currentKey);
-    process.waitForFinished(-1); // will wait forever until finished
+    process.waitForFinished(-1);
     QString cmd_stdout = process.readAllStandardOutput();
     if (cmd_stdout.length() == 0) {
        QString cmd_stderr = process.readAllStandardError();
@@ -69,4 +76,46 @@ void MainWindow::on_pushButton_released()
 void MainWindow::on_actionQuit_triggered()
 {
     exit(0);
+}
+
+void MainWindow::on_actionAdd_New_triggered()
+{
+    bool ok1, ok2;
+    QString text1 = QInputDialog::getText(this, tr("QInputDialog::getText()"),
+                                        tr("2FA key name:"), QLineEdit::Normal,
+                                        "", &ok1);
+    if (!ok1) {
+        return;
+    }
+    QString text2 = QInputDialog::getText(this, tr("QInputDialog::getText()"),
+                                        tr("secret code:"), QLineEdit::Normal,
+                                        "", &ok2);
+    if (ok1 && !text1.isEmpty() && ok2 && !text2.isEmpty()){
+        QProcess process;
+        // start the add
+        process.start("2fa -add " + text1);
+        // write user input to stdin
+        process.write(text2.toUtf8()+"\r\n");
+        process.waitForFinished(-1);
+
+        // handle non-zero exit code
+        QString cmd_stderr = process.readAllStandardError().replace("2fa key for "+text1+":", "");
+        cmd_stderr = cmd_stderr.trimmed();
+        if (process.exitCode() != 0 || cmd_stderr.length() != 0) {
+           QMessageBox messageBox;
+           messageBox.critical(nullptr, "Error " + QString::number(process.exitCode()), "Key not added. " + cmd_stderr);
+        } else {
+            this->repopulate();
+        }
+    }
+}
+
+void MainWindow::on_actionAbout_triggered()
+{
+    QMessageBox messageBox;
+    messageBox.setTextFormat(Qt::RichText);
+    messageBox.setTextInteractionFlags(Qt::TextBrowserInteraction);
+    messageBox.setText("<p><i>Simple QT Frontend for 2fa command.</i></p><p>Source code: <a href=\"https://github.com/aerth/2fa-qt\">https://github.com/aerth/2fa-qt</a></p><p>License: MIT</p>");
+    messageBox.setWindowTitle("About 2fa-qt");
+    messageBox.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,7 @@
 #include <QProcess>
 #include <QMessageBox>
 #include <QClipboard>
+#include <QInputDialog>
 
 namespace Ui {
 class MainWindow;
@@ -22,6 +23,9 @@ private slots:
     void on_comboBox_activated(const QString &arg1);
     void on_pushButton_released();
     void on_actionQuit_triggered();
+    void on_actionAdd_New_triggered();
+    void repopulate();
+    void on_actionAbout_triggered();
 
 private:
     QString currentKey = nullptr;
